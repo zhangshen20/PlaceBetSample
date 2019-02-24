320b96468fcefbfe7a173b47ba408f92df74d093
zhangshen20 <zhangshen20@hotmail.com>
2019-02-25 09:47:20 +1100

    V3.0 Add Sportsbet API - Testing stage

---
+++ PlaceBet/BetSportsBet.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Collections.Concurrent;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Net.Mail;
+using System.Configuration;
+
+namespace PlaceBet
+{
+    class BetSportsBet
+    {
+        public string TabcorpAuth { get; set; }
+        public string AccountNumber { get; set; }
+        public string PropositionId { get; set; }
+        public string Odds { get; set; }
+        public string Stake { get; set; }
+        public string BetType { get; set; }
+
+        public string URLAuthenticate { get; set; }
+        public string URLBalance { get; set; }
+        public string URLBetslipEnquiry { get; set; }
+        public string URLBetslip { get; set; }
+        public string UUIDV4 { get; set; }
+
+        public BetSportsBet(string accountnubmer, string token, string propositionid, string stake, string odds, string bettype)
+        {
+            this.AccountNumber = accountnubmer;
+            this.TabcorpAuth = token;
+            this.PropositionId = propositionid;
+            this.Stake = String.Format("${0:F2}", (Convert.ToDecimal(stake)));
+            this.Odds = String.Format("{0:F2}", (Convert.ToDecimal(odds)));
+            this.BetType = bettype;
+
+            Console.WriteLine("this.Stake -> " + this.Stake);
+            Console.WriteLine("this.Odds -> " + this.Odds);
+
+            this.URLAuthenticate = @"https://www.sportsbet.com.au/apigw/auth/realms/sportsbet/protocol/openid-connect/token";
+
+            //this.URLBalance = @"https://webapi.tab.com.au/v1/account-service/tab/accounts/" + this.AccountNumber + @"/balance";
+            //this.URLBetslipEnquiry = @"https://webapi.tab.com.au/v1/tab-betting-service/accounts/" + this.AccountNumber + @"/betslip-enquiry?TabcorpAuth=" + this.TabcorpAuth;
+            //this.URLBetslip = @"https://webapi.tab.com.au/v1/tab-betting-service/accounts/" + this.AccountNumber + @"/betslip?TabcorpAuth=" + this.TabcorpAuth;
+        }
+
+        public void POSTingBetSlipEnquiry()
+        {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(this.URLBetslipEnquiry);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = CreatePayloadBetSlipEnquiry();
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+
+                    APITABBetSlipEnquiryResponse betSlipEnquiryResponse = JsonConvert.DeserializeObject<APITABBetSlipEnquiryResponse>(result);
+                    this.Odds = betSlipEnquiryResponse.bets.First().legs.First().odds;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public Boolean POSTingBetSlip()
+        {
+            Boolean bPlaceBetSucceed = false;
+
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(this.URLBetslip);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = CreatePayloadBetSlip();
+
+                    Console.WriteLine(" ----------------------------------  ");
+                    Console.WriteLine(" json payload \t {0} ", json);
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+
+                    APITABBetSlipResponse betSlipResponse = JsonConvert.DeserializeObject<APITABBetSlipResponse>(result);
+                    //this.Odds = betSlipResponse.bets.First().legs.First().odds;
+                    if (betSlipResponse.errors.Count == 0)
+                    {
+                        bPlaceBetSucceed = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ----------------------------------  ");
+                        Console.WriteLine(" Error: (code)          \t {0} ", betSlipResponse.errors.First<APITABBetSlipResponse._errors>().code);
+                        Console.WriteLine(" Error: (message)       \t {0} ", betSlipResponse.errors.First<APITABBetSlipResponse._errors>().message);
+                        Console.WriteLine(" Error: (odds)          \t {0} ", betSlipResponse.errors.First<APITABBetSlipResponse._errors>().odds);
+                        Console.WriteLine(" Error: (oldOdds)       \t {0} ", betSlipResponse.errors.First<APITABBetSlipResponse._errors>().oldOdds);
+                        Console.WriteLine(" Error: (propositionId) \t {0} ", betSlipResponse.errors.First<APITABBetSlipResponse._errors>().propositionId);
+                    }
+
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(" ----------------------------------  ");
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                //Console.WriteLine(" ----------------------------------  ");
+                //Console.WriteLine("Error at POSTingBetSlip() -> " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" ----------------------------------  ");
+                Console.WriteLine("Error at POSTingBetSlip() -> " + e.Message);
+            }
+
+            return bPlaceBetSucceed;
+        }
+
+        public string CreatePayloadBetSlipEnquiry()
+        {
+            string sPayload = @"
+                        {
+                          ""bets"": [
+                            {
+                              ""type"": ""FIXED_ODDS"",
+                              ""stake"": """ + this.Stake + @""",
+                              ""legs"": [
+                                {
+                                  ""type"": ""WIN"",
+                                  ""propositionId"": " + this.PropositionId + @",
+                                  ""odds"": """ + this.Odds + @"""
+                                }
+                              ],
+                              ""enableMultiplier"": false,
+                              ""source"": ""racing.race-nav.race.bet-type""
+                            }
+                          ]
+                        }
+            ";
+
+            return sPayload;
+        }
+
+        public string GenerateUUIDV4()
+        {
+            Guid g = Guid.NewGuid();
+            return g.ToString();
+        }
+
+        public string CreatePayloadBetSlip()
+        {
+            this.UUIDV4 = GenerateUUIDV4();
+
+            string sPayload = @"
+                        {
+                          ""transactionId"": """ + this.UUIDV4 + @""",
+                          ""bets"": [
+                            {
+                              ""type"": ""FIXED_ODDS"",
+                              ""stake"": """ + this.Stake + @""",
+                              ""legs"": [
+                                {
+                                  ""type"": ""WIN"",
+                                  ""propositionId"": " + this.PropositionId + @",
+                                  ""odds"": """ + this.Odds + @"""
+                                }
+                              ],
+                              ""enableMultiplier"": false,
+                              ""source"": ""racing.race-nav.race.bet-type""
+                            }
+                          ]
+                        }
+            ";
+
+            return sPayload;
+        }
+    }
+}
--- PlaceBet/Form1.cs
+++ PlaceBet/Form1.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using System.Web;
+using RestSharp;
 
 namespace PlaceBet
 {
@@ -18,7 +20,8 @@ namespace PlaceBet
         public string Token { get; set; }
         public string AccountNumber { get; set; }
         public string Password { get; set; }
-        public string URLAuthenticate { get; set; }
+        public string URLAuthenticateTAB { get; set; }
+        public string URLAuthenticateSportsBet { get; set; }
 
         System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
 
@@ -35,7 +38,8 @@ namespace PlaceBet
 
             this.Password = this.textBoxPassword.Text;
             this.AccountNumber = this.textBoxAccountNumber.Text;
-            this.URLAuthenticate = @"https://webapi.tab.com.au/v1/account-service/tab/authenticate";
+            this.URLAuthenticateTAB = @"https://webapi.tab.com.au/v1/account-service/tab/authenticate";
+            this.URLAuthenticateSportsBet = @"https://www.sportsbet.com.au/apigw/auth/realms/sportsbet/protocol/openid-connect/token";
 
             Console.WriteLine(" ----------------------------------  ");
             Console.WriteLine("Timer starts ");
@@ -46,9 +50,10 @@ namespace PlaceBet
         {
             this.Password = this.textBoxPassword.Text;
             this.AccountNumber = this.textBoxAccountNumber.Text;
-            this.URLAuthenticate = @"https://webapi.tab.com.au/v1/account-service/tab/authenticate";
+            this.URLAuthenticateTAB = @"https://webapi.tab.com.au/v1/account-service/tab/authenticate";
+            this.URLAuthenticateSportsBet = @"https://www.sportsbet.com.au/apigw/auth/realms/sportsbet/protocol/openid-connect/token";
 
-            GetAuthToken();
+            GetAuthTokenSportsBet();
         }
 
         private void TimerValidateAuthToken(Object myObject, EventArgs myEventArgs)
@@ -60,7 +65,7 @@ namespace PlaceBet
         {
             if(gAuthToken.AbsoluteExpiryDateTimeUTC.AddSeconds(-1 * gTimerOffsetInSecs) <= DateTime.UtcNow)
             {
-                GetAuthToken();
+                GetAuthTokenSportsBet();
 
                 Console.WriteLine(" ----------------------------------  ");
                 Console.WriteLine("New Auth Token generated");
@@ -78,23 +83,23 @@ namespace PlaceBet
             }
         }
 
-        public string CreatePayloadAuthenticate()
+        public string CreatePayloadAuthenticateTAB()
         {
             string sPayload = @"{""password"":""" + this.Password + @""",""channel"":""TABCOMAU"",""scopes"":["" * ""],""extendedTokenLifeTime"":true,""accountNumber"": " + this.AccountNumber + "}";
             return sPayload;
         }
 
-        public void GetAuthToken()
+        public void GetAuthTokenTAB()
         {
             try
             {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(this.URLAuthenticate);
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(this.URLAuthenticateTAB);
                 httpWebRequest.ContentType = "application/json";
                 httpWebRequest.Method = "POST";
 
                 using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                 {
-                    string json = CreatePayloadAuthenticate();
+                    string json = CreatePayloadAuthenticateTAB();
 
                     streamWriter.Write(json);
                     streamWriter.Flush();
@@ -116,6 +121,93 @@ namespace PlaceBet
             catch(Exception e)
             {
 
+            }
+        }
+
+        public string CreatePayloadAuthenticateSportsBet()
+        {
+            string sPayload = "client_id=cxp&grant_type=password&username=jackyzhang1981&password=Shenzhang123&scope=offline_access";
+            //string sPayload = HttpUtility.UrlEncode("client_id=cxp&grant_type=password&username=jackyzhang1981&password=Shenzhang123");
+            return sPayload;
+        }
+
+        public void GetAuthTokenSportsBet()
+        {
+            try
+            {
+                //var client = new RestClient(@"https://www.sportsbet.com.au/apigw/auth/realms/sportsbet/protocol/openid-connect/token");
+                //var request = new RestRequest(Method.POST);
+
+                //request.AddHeader("Postman-Token", "bce6bd43-f684-4162-9e21-728f11938dad");
+                //request.AddHeader("cache-control", "no-cache");
+                //request.AddHeader("cookie", @"cust_lang=en; aid=8575744680914864; _ga=GA1.3.1848199981.1531856942; sbt_banner_cookie=1; optimizelyEndUserId=oeu1537716251577r0.3532249167138428; __zlcmid=oXhfEcjtNfgNOd; NaN_hash=ae29bf59WXGOHZAF1540292697941; sb_partner_mid=qF3bS5LVKIbulQmTAVK722Nd7ZgqdRLk12921; _gid=GA1.3.1714688751.1550894255; _gcl_au=1.1.2063565678.1550894256; _fbp=fb.2.1550894255916.672290551; tl_clid=1; tl_evtid=4526152; tl_cmpid=602; breakpoint=narrow; NJ_POI=e769089ee3d34add816b7f30f95efcae; tl_cid=1080031; OB_REQ=3e16098016ebe97c8350; OB_REQ_NONSSL=6d9bcb8610aab2daebf0; isLoggedIn=false; AKA_A2=A; utag_main=v_id:0164a9c9f21b002358b45ac4eec403072001d06a00bd0$_sn:13$_ss:0$_st:1550983801722$dc_visit:13$_prevpage:%2Flogin%3Bexp-1550985601726$ses_id:1550981767215%3Bexp-session$_pn:1%3Bexp-session$dc_event:7%3Bexp-session$dc_region:ap-southeast-2%3Bexp-session");
+                ////request.AddHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36");
+                //request.AddHeader("user-agent", "RestSharp / 105.0.1.0");                
+
+                //request.AddHeader("content-type", "application/x-www-form-urlencoded");                
+                //request.AddParameter("application/x-www-form-urlencoded", "client_id=cxp&grant_type=password&username=jackyzhang1981&password=Shenzhang123&scope=offline_access", ParameterType.RequestBody);
+
+                ////request.AddHeader("Accept", "application/json");
+                ////request.AddHeader("content-type", "application/json");
+                ////request.Parameters.Clear();
+                ////request.AddParameter("application/json", "{ \"client_id\": \"cxp\", \"grant_type\":\"password\",\"username\":\"jackyzhang1981\",\"password\":\"Shenzhang123\",\"scope\":\"offline_access\"} ", ParameterType.RequestBody);
+
+                ////request.AddHeader("content-type", "application/json");
+                ////request.AddParameter("application/json", "{ \"client_id\": \"cxp\", \"grant_type\":\"password\",\"username\":\"jackyzhang1981\",\"password\":\"Shenzhang123\",\"scope\":\"offline_access\"} ", ParameterType.RequestBody);
+
+
+                //IRestResponse response = client.Execute(request);
+
+                //if (response.StatusCode != HttpStatusCode.OK)
+                //{
+                //    Console.WriteLine("Access Token cannot obtain, process terminate");
+                //    return;
+                //}
+
+                Console.WriteLine(" *************** ");
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(this.URLAuthenticateSportsBet);
+                httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
+                //httpWebRequest.UserAgent = ".NET Framework";
+                //httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                //httpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36";
+                //httpWebRequest.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+
+                //httpWebRequest.KeepAlive = false;
+                //httpWebRequest.ProtocolVersion = HttpVersion.Version10;
+                //httpWebRequest.ServicePoint.ConnectionLimit = 1;
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                //using (var streamWriter = httpWebRequest.GetRequestStream())
+                {
+                    string str = CreatePayloadAuthenticateSportsBet();
+                    //byte[] byteArray = Encoding.ASCII.GetBytes(str);
+
+                    //streamWriter.Write(byteArray, 0, byteArray.Length);
+                    streamWriter.Write(str);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+
+                    APITABAuthenticateResponse authencateResponse = JsonConvert.DeserializeObject<APITABAuthenticateResponse>(result);
+                    gAuthToken.Token = authencateResponse.authentication.token;
+                    gAuthToken.UpdateAbsoluteExpiryDateTimeUTC(authencateResponse.authentication.absoluteExpiry);
+                    gAuthToken.UpdateInactivityExpiryDateTimeUTC(authencateResponse.authentication.inactivityExpiry);
+                    this.textWebResponse.Text = gAuthToken.Token;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" exception -> " + e.ToString());
+                MessageBox.Show(e.Message);
             }
         }
 
